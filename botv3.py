21115cb94f6a4bfdafb530e615cc0b5aac9addbf
madityaar <m.adityarayhan@yahoo.co.id>
2019-06-26 12:46:30 +0700

    push rapihin

--- botv3.py
+++ botv3.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import json 
 import requests
 import urllib.request
@@ -35,24 +29,24 @@ def insert_row(data):
     cur.execute("INSERT INTO tiket (no_tiket,gambar_sebelum,gambar_sesudah,keterangan,latitude,longitude) VALUES ('"+data['no_tiket']+"','"+data['gambar_sebelum']+"','"+data['gambar_sesudah']+"','"+data['keterangan']+"','"+data['latitude'] +"','"+data['longitude']+"')")
     db.commit()
     db.close()
+    
 ###########
 
-data2={"keyboard": [["/input_tiket"]],"one_time_keyboard": True}
-data4={"keyboard": [["/input_keterangan"],["/input_gambar_sebelum"],["/input_gambar_progres"],["/input_gambar_sesudah"],["/input_lokasi"],["/review_tiket"]],"one_time_keyboard": True,"resize_keyboard": True}
-data5={"keyboard": [[{"text":"kirim lokasi","request_location":True}]],"one_time_keyboard": True,"resize_keyboard": True}
-data6={"keyboard": [["Edit Data"],["/input_selesai"]],"one_time_keyboard": True}
+data1={"keyboard": [["/input_tiket"]],"one_time_keyboard": True}
+data2={"keyboard": [["/input_keterangan"],["/input_gambar_sebelum"],["/input_gambar_progres"],["/input_gambar_sesudah"],["/input_lokasi"],["/review_tiket"]],"one_time_keyboard": True,"resize_keyboard": True}
+data3={"keyboard": [[{"text":"kirim lokasi","request_location":True}]],"one_time_keyboard": True,"resize_keyboard": True}
+data4={"keyboard": [["Edit Data"],["/input_selesai"]],"one_time_keyboard": True}
 
-data_json=json.dumps(data2)
-data_json3=json.dumps(data4)
-data_json4=json.dumps(data5)
-data_json5=json.dumps(data6)
+json_input_tiket=json.dumps(data1)
+json_all_comm=json.dumps(data2)
+json_req_location=json.dumps(data3)
+json_aft_review=json.dumps(data4)
 
 def reply_keyboard(text,chatId,djson):
     url = URL + "sendMessage?text={}&chat_id={}&reply_markup={}".format(text,chatId, djson)
     get_url(url)
     
 def remove_keyboard(text, chatId):
-   
     json_remove=json.dumps({"remove_keyboard":True})
     url = URL + "sendMessage?text={}&chat_id={}&reply_markup={}".format(text,chatId, json_remove)
     get_url(url)
@@ -107,6 +101,7 @@ class Tiket():
         self.latitude= latitude
         self.longitude= longitude
         self.state= state
+        
     def setChatId(self,chatId):
         self.chatId = chatId
     
@@ -126,20 +121,6 @@ class Tiket():
             self.gambarProgres = str(fileId)
         elif(nama=='sesudah'):
             self.gambarSesudah = str(fileId)
-    
-    def setIsiTiket(self,update):
-        print(update.tipeKonten)
-        if(update.tipeKonten=="text"):
-            if "/input_keterangan" in update.konten[0]:
-                self.setKeterangan(update.konten[0].lstrip("/input_keterangan").strip())
-                send_message(self.chatId,"Keterangan berhasil terinput")
-        elif(update.tipeKonten=="gambar"):
-            if "/input_gambar" in update.konten[1]:
-                self.setGambar(update.konten[0],update.konten[1].lstrip("/input_gambar").strip())
-        elif(update.tipeKonten=="location"):
-            print(update.konten)
-            self.setLokasi(update.konten[0],update.konten[1])
-            send_message(self.chatId,"Lokasi berhasil terinput")
             
     def setLokasi(self,latitude,longitude):
         self.latitude=latitude
@@ -163,15 +144,15 @@ class Tiket():
         except FileExistsError:
             True
         try:
-            save_file(self.gambarSebelum,self.chatId,self.noTiket,'sebelum')
-            save_file(self.gambarProgres,self.chatId,self.noTiket,'progres')
-            save_file(self.gambarSesudah,self.chatId,self.noTiket,'sesudah')
-            save_text(self.keterangan,self.chatId,self.noTiket,'keterangan')
+            self.save_file(self.gambarSebelum,'sebelum')
+            self.save_file(self.gambarProgres,'progres')
+            self.save_file(self.gambarSesudah,'sesudah')
+            self.save_text(self.keterangan,'keterangan')
             
-            loc_lengkap='Latitude: '+str(self.latitude)+' \nLongitude: '+str(self.longitude)+' \nDistance: '+str(calc_distance(self.latitude,self.longitude))
-            save_text(loc_lengkap,self.chatId,self.noTiket,'lokasi')
+            loc_lengkap='Latitude: '+str(self.latitude)+' \nLongitude: '+str(self.longitude)+' \nDistance: '+str(self.calc_distance())
+            self.save_text(loc_lengkap,'lokasi')
                                 
-            send_message(self.chatId,"Tiket dengan No. "+self.noTiket+" selesai diinputkan")
+            self.send_message("Tiket dengan No. "+self.noTiket+" selesai diinputkan")
             
             self.noTiket = ''
             self.keterangan = '' 
@@ -182,73 +163,75 @@ class Tiket():
             self.longitude= ''
             self.state= 'none'
         except KeyError:
-            send_message(self.chatId, "Data yang diinputkan belum lengkap")
+            self.send_message("Data yang diinputkan belum lengkap")
     
     def reviewTiket(self):
-        send_message(self.chatId,"Isi tiket")
-        send_message(self.chatId,"noTiket: "+self.noTiket)
+        self.send_message("Isi tiket")
+        self.send_message("noTiket: "+self.noTiket)
         status=True
         if(self.keterangan!=""):
-            send_message(self.chatId,"keterangan: "+self.keterangan)
+            self.send_message("keterangan: "+self.keterangan)
         else:
             status=False
         if(self.gambarSebelum!=""):
-            send_file(self.chatId,self.gambarSebelum,"Gambar Sebelum")
+            self.send_file(self.gambarSebelum,"Gambar Sebelum")
         else:
             status=False
         if(self.gambarProgres!=""):
-            send_file(self.chatId,self.gambarProgres,"Gambar Progres")
+            self.send_file(self.gambarProgres,"Gambar Progres")
         else:
             status=False
         if(self.gambarSesudah!=""):
-            send_file(self.chatId,self.gambarSesudah,"Gambar Sesudah")
+            self.send_file(self.gambarSesudah,"Gambar Sesudah")
         else:
             status=False
         if(self.latitude!=""):
-            send_location(self.chatId,self.latitude,self.longitude)
+            self.send_location()
         else:
             status=False
         if(status):   
-            reply_keyboard("Edit Kembali atau Submit?",self.chatId,data_json5)
+            reply_keyboard("Edit Kembali atau Submit?",self.chatId,json_aft_review)
         else:
-            reply_keyboard("Data belum lengkap. Sila lengkapi kembali",self.chatId,data_json3)
-
-def save_file(fileId, chat_id,noTiket,namaFile):
-    url = URL + "getFile?file_id={}".format(fileId)
-    js = get_json_from_url(url)
-    file_path = js["result"]["file_path"]
-    url = URL_FILE + file_path
-    testfile = urllib.request.urlretrieve(url,str(noTiket)+"/"+str(noTiket)+"-"+str(namaFile)+".jpg")
-    
-def save_text(text, chat_id,path, namafile):
-    file = open(str(path)+"/"+namafile+".txt","w") 
-    file.write(text)
-    file.close()
-    
-def calc_distance(lat1, lon1):
-    lat2=-6.229697
-    lon2=106.816049
-    R = 6371e3  #metres
-    o1 = math.radians(lat1)
-    o2 = math.radians(lat2)
-    delta1 = math.radians(lat2-lat1)
-    delta2 = math.radians(lon2-lon1)
-
-    a = math.sin(delta1/2) * math.sin(delta1/2) + math.cos(o1) * math.cos(o2) * math.sin(delta2/2) * math.sin(delta2/2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    d = R * c
-    return(d)
-def send_message(chatId,text):
-    url = URL + "sendMessage?text={}&chat_id={}".format(text, chatId)
-    get_url(url)
+            reply_keyboard("Data belum lengkap. Sila lengkapi kembali",self.chatId,json_all_comm)
         
-def send_file(chatId, file_id,text):
-    url = URL + "sendPhoto?photo={}&chat_id={}&caption={}".format(file_id, chatId,text)
-    get_url(url)
-
-def send_location(chatId, lat, long):
-    url = URL + "sendlocation?chat_id={}&latitude={}&longitude={}".format(chatId, lat, long)
-    get_url(url)
+    def save_file(self, fileId, namaFile):
+        url = URL + "getFile?file_id={}".format(fileId)
+        js = get_json_from_url(url)
+        file_path = js["result"]["file_path"]
+        url = URL_FILE + file_path
+        urllib.request.urlretrieve(url,str(self.noTiket)+"/"+str(self.noTiket)+"-"+str(namaFile)+".jpg")
+        
+    def save_text(self, namafile):
+        file = open(str(self.noTiket)+"/"+namafile+".txt","w") 
+        file.write(self.keterangan)
+        file.close()
+        
+    def calc_distance(self):
+        lat2=-6.229697
+        lon2=106.816049
+        R = 6371e3  #metres
+        o1 = math.radians(self.latitude)
+        o2 = math.radians(lat2)
+        delta1 = math.radians(lat2-self.latitude)
+        delta2 = math.radians(lon2-self.longitude)
+    
+        a = math.sin(delta1/2) * math.sin(delta1/2) + math.cos(o1) * math.cos(o2) * math.sin(delta2/2) * math.sin(delta2/2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+        d = R * c
+        return(d)
+        
+    def send_message(self, text):
+        url = URL + "sendMessage?text={}&chat_id={}".format(text, self.chatId)
+        get_url(url)
+            
+    def send_file(self, file_id,text):
+        url = URL + "sendPhoto?photo={}&chat_id={}&caption={}".format(file_id, self.chatId,text)
+        get_url(url)
+    
+    def send_location(self):
+        url = URL + "sendlocation?chat_id={}&latitude={}&longitude={}".format(self.chatId, self.latitude, self.longitude)
+        get_url(url)
+        
 
 def get_url(url):
     response = requests.get(url)
@@ -279,81 +262,76 @@ def main():
                 if(tiket[update.chatId].state=="none"):
                     if "/input_tiket" in update.konten[0]:
                         tiket[update.chatId].setState("notiket")
-                        remove_keyboard("Silahkan input nomor tiket",update.chatId)
+                        remove_keyboard("Silakan input nomor tiket",update.chatId)
                     if(tiket[update.chatId].noTiket!=''):
                         if "/input_keterangan" in update.konten[0]:
                             tiket[update.chatId].setState("keterangan")
-                            remove_keyboard("Silahkan input keterangan",update.chatId)
+                            remove_keyboard("Silakan input keterangan",update.chatId)
                         elif "/input_gambar_sebelum" in update.konten[0]:
                             tiket[update.chatId].setState("gambar sebelum")
-                            remove_keyboard("Silahkan input gambar",update.chatId)
+                            remove_keyboard("Silakan input gambar",update.chatId)
                         elif "/input_gambar_progres" in update.konten[0]:
                             tiket[update.chatId].setState("gambar progres")
-                            remove_keyboard("Silahkan input gambar",update.chatId)
+                            remove_keyboard("Silakan input gambar",update.chatId)
                         elif "/input_gambar_sesudah" in update.konten[0]:
                             tiket[update.chatId].setState("gambar sesudah")
-                            remove_keyboard("Silahkan input gambar",update.chatId)
+                            remove_keyboard("Silakan input gambar",update.chatId)
                         elif "/input_lokasi" in update.konten[0]:
                             tiket[update.chatId].setState("lokasi")
-                            reply_keyboard("Silahkan input lokasi",update.chatId,data_json4)
+                            reply_keyboard("Silakan input lokasi",update.chatId,json_req_location)
                         elif "/input_selesai" in update.konten[0]:
                             tiket[update.chatId].saveData()
                         elif "/review_tiket" in update.konten[0]:
                             tiket[update.chatId].reviewTiket()
                         elif "Edit Data" in update.konten[0]:
-                            reply_keyboard("Silahkan pilih data yang akan diedit",update.chatId,data_json3)
+                            reply_keyboard("Silakan pilih data yang akan diedit",update.chatId,json_all_comm)
                         else:
-                            send_message(update.chatId,"Silahkan masukkan command terlebih dahulu")
+                            tiket[update.chatId].send_message("Silakan masukkan perintah yang sesuai")
                     elif (tiket[update.chatId].state!="notiket"):
-                        reply_keyboard("Silahkan masukkan nomor tiket terlebih dahulu",update.chatId,data_json)
+                        reply_keyboard("Silakan masukkan nomor tiket terlebih dahulu",update.chatId,json_input_tiket)
                 else:
                     if(tiket[update.chatId].state=="notiket"):
                         if(update.tipeKonten=="text"):
                             tiket[update.chatId].setNoTiket(update.konten[0])
-                            reply_keyboard("Nomor Tiket: "+tiket[update.chatId].noTiket+" berhasil diinput",update.chatId,data_json3)
+                            reply_keyboard("Nomor Tiket: "+tiket[update.chatId].noTiket+" berhasil diinput",update.chatId,json_all_comm)
                             tiket[update.chatId].setState("none")
                         else:
-                            send_message(update.chatId,"Nomor tiket harus berupa teks")
+                            tiket[update.chatId].send_message("Nomor tiket harus berupa teks")
                     elif(tiket[update.chatId].state=="keterangan"):
                         if(update.tipeKonten=="text"):
                             tiket[update.chatId].setKeterangan(update.konten[0])
-            
-                            reply_keyboard("Keterangan berhasil terinput",update.chatId,data_json3)
+                            reply_keyboard("Keterangan berhasil terinput",update.chatId,json_all_comm)
                             tiket[update.chatId].setState("none")
                         else:
-                            send_message(update.chatId,"Isi keterangan harus berupa teks")
+                            tiket[update.chatId].send_message("Isi keterangan harus berupa teks")
                     elif(tiket[update.chatId].state=="gambar sebelum"):
                         if(update.tipeKonten=="gambar"):
                             tiket[update.chatId].setGambar(update.konten[0],"sebelum")
-                            reply_keyboard("Gambar tampak 'sebelum' berhasil terinput",update.chatId,data_json3)
-                            #send_message(update.chatId,"Gambar tampak 'sebelum' berhasil terinput")
+                            reply_keyboard("Gambar tampak 'sebelum' berhasil terinput",update.chatId,json_all_comm)
                             tiket[update.chatId].setState("none")
                         else:
-                            send_message(update.chatId,"Input gambar harus berupa gambar")
+                            tiket[update.chatId].send_message("Input gambar harus berupa gambar")
                     elif(tiket[update.chatId].state=="gambar progres"):
                         if(update.tipeKonten=="gambar"):
                             tiket[update.chatId].setGambar(update.konten[0],"progres")
-                            reply_keyboard("Gambar tampak 'progres' berhasil terinput",update.chatId,data_json3)
-                            #send_message(update.chatId,"Gambar tampak 'progres' berhasil terinput")
+                            reply_keyboard("Gambar tampak 'progres' berhasil terinput",update.chatId,json_all_comm)
                             tiket[update.chatId].setState("none")
                         else:
-                            send_message(update.chatId,"Input gambar harus berupa gambar")
+                            tiket[update.chatId].send_message("Input gambar harus berupa gambar")
                     elif(tiket[update.chatId].state=="gambar sesudah"):
                         if(update.tipeKonten=="gambar"):
                             tiket[update.chatId].setGambar(update.konten[0],"sesudah")
-                            reply_keyboard("Gambar tampak 'sesudah' berhasil terinput",update.chatId,data_json3)
-                            #send_message(update.chatId,"Gambar tampak 'sesudah' berhasil terinput")
+                            reply_keyboard("Gambar tampak 'sesudah' berhasil terinput",update.chatId,json_all_comm)
                             tiket[update.chatId].setState("none")
                         else:
-                            send_message(update.chatId,"Input gambar harus berupa gambar")
+                            tiket[update.chatId].send_message("Input gambar harus berupa gambar")
                     elif(tiket[update.chatId].state=="lokasi"):
                         if(update.tipeKonten=="location"):
                             tiket[update.chatId].setLokasi(update.konten[0],update.konten[1])
-                            reply_keyboard("Lokasi berhasil terinput",update.chatId,data_json3)
-                            #send_message(update.chatId,"Lokasi berhasil terinput")
+                            reply_keyboard("Lokasi berhasil terinput",update.chatId,json_all_comm)
                             tiket[update.chatId].setState("none")
                         else:
-                            send_message(update.chatId,"Input lokasi harus berupa lokasi")
+                            tiket[update.chatId].send_message("Input lokasi harus berupa lokasi")
                 try:
                     tiket[update.chatId].print_all()
                 except AttributeError:
